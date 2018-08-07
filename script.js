55948fa481b8061516b0dd8b3bf632cb35298f48
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-08-07 16:15:37 -0500

    checkerboard working

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 // Your JS goes here
+console.log("connected");
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+function createPair () {
+    const square = document.createElement('div');
+    square.style.backgroundColor = 'black';
+    square.style.width = "11.1%";
+    square.style.height= '20 px'; 
+    square.style.cssFloat= 'left';
+    square.style.paddingBottom = '11.1%'
+    square.className = 'blackSquare';
+    document.body.appendChild(square);
+    
+    const redSquare = document.createElement('div');
+    redSquare.style.backgroundColor = 'red';
+    redSquare.style.width = "11.1%";
+    redSquare.style.height= '20 px'; 
+    redSquare.style.cssFloat= 'left';
+    redSquare.style.paddingBottom = '11.1%'
+    redSquare.className = 'redSquare';
+    document.body.appendChild(redSquare);
+}
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();    
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    createPair();
+    
+
+})
