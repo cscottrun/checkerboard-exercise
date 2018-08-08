53f11c15e14cd8afc6d765bedaf8161d1cbadf28
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-08-07 21:20:20 -0500

    this is the real gradients

--- script.js
+++ script.js
@@ -2,39 +2,38 @@
 console.log("connected");
 
 document.addEventListener('DOMContentLoaded',()=>{
-   makeSquares();
-   window.setInterval(changeColor,2000);
+    const bigSquare = document.createElement('div');
+    document.body.appendChild(bigSquare);
+    bigSquare.style.width = "99.9%";
+    bigSquare.style.cssFloat= 'left';
+    bigSquare.style.paddingBottom = '85%'
+    bigSquare.style.backgroundColor = 'blue';
+    bigSquare.style.position = 'absolute'
     
-})
-
-function changeColor () {
-    let squares = document.getElementsByTagName('div');
-    for (let square of squares) {
-    square.style.background = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-}
-}
-
-function makeSquares(){
     let count = 0;
-    while (count < 72) {   
+    let opacity = 0;
+    while (count < 36) {   
     const square = document.createElement('div');
     document.body.appendChild(square);
     square.style.width = "11.1%";
     square.style.cssFloat= 'left';
     square.style.paddingBottom = '11.1%'
-    square.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+    square.style.backgroundColor = 'aqua';
+    square.style.opacity = `${opacity}`;
     
+    const pinkSquare = document.createElement('div');
+    document.body.appendChild(pinkSquare);
+    pinkSquare.style.width = "11.1%";
+    pinkSquare.style.cssFloat= 'left';
+    pinkSquare.style.paddingBottom = '11.1%'
+    pinkSquare.style.backgroundColor = 'fuchsia';
+    pinkSquare.style.opacity = `${opacity}`
+   
+    opacity += 1/36;
     count ++;
+    
     }
-}  
+    
+})
 
 
-
-
- // const bigSquare = document.createElement('div');
-    // document.body.appendChild(bigSquare);
-    // bigSquare.style.width = "99.9%";
-    // bigSquare.style.cssFloat= 'left';
-    // bigSquare.style.paddingBottom = '85%'
-    // bigSquare.style.backgroundColor = 'blue';
-    // bigSquare.style.position = 'absolute'
