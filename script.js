30b98444173c7a3d95b97caaac1cec439936e943
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-08-07 21:13:20 -0500

    not sure what this is commiting, but hope its gradients

--- script.js
+++ script.js
@@ -2,21 +2,39 @@
 console.log("connected");
 
 document.addEventListener('DOMContentLoaded',()=>{
+   makeSquares();
+   window.setInterval(changeColor,2000);
+    
+})
+
+function changeColor () {
+    let squares = document.getElementsByTagName('div');
+    for (let square of squares) {
+    square.style.background = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+}
+}
+
+function makeSquares(){
     let count = 0;
     while (count < 72) {   
     const square = document.createElement('div');
     document.body.appendChild(square);
     square.style.width = "11.1%";
-    square.style.height= '20 px'; 
     square.style.cssFloat= 'left';
     square.style.paddingBottom = '11.1%'
-    square.className = 'blackSquare';
-    square.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255} , ${Math.random()*255})`;
-   
+    square.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+    
     count ++;
-    
-}
-    
-})
+    }
+}  
 
 
+
+
+ // const bigSquare = document.createElement('div');
+    // document.body.appendChild(bigSquare);
+    // bigSquare.style.width = "99.9%";
+    // bigSquare.style.cssFloat= 'left';
+    // bigSquare.style.paddingBottom = '85%'
+    // bigSquare.style.backgroundColor = 'blue';
+    // bigSquare.style.position = 'absolute'
